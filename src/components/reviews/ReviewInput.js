4bacb364c37eb29cd6b33e4162966c2220e4b033
thedannysoto <87dsoto@gmail.com>
2020-06-08 18:56:37 -0500

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.addReview(this.state.text, this.props.restaurantId);
+    this.setState({
+      text: ''
+    })
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        Review Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input 
+            type="text" 
+            value={this.state.text}
+            onChange={event => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
 
+import { connect } from 'react-redux'
+
 class ReviewsContainer extends Component {
 
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput addReview={this.props.addReview} restaurantId={this.props.restaurant.id}/>
+        <Reviews reviews={this.props.reviews} restaurant={this.props.restaurant} deleteReview={this.props.deleteReview}/>
       </div>
     )
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = ({ reviews }) => ({ reviews })
+
+const mapDispatchToProps = dispatch => ({
+  addReview: (text, restaurantId) => dispatch({ type: "ADD_REVIEW", text, restaurantId }),
+  deleteReview: id => dispatch({ type: "DELETE_REVIEW", id })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
